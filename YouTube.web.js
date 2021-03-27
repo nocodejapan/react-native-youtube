6a8d59959d7729fd17ff1b25f65c2f07a5319fb1
Thanh Tran <thanh.tran@sotatek.com>
2021-03-27 19:29:42 +0700

    Add autoplay and origin to youtube web

--- YouTube.web.js
+++ YouTube.web.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const YouTube = ({ videoId, style }) => {
+const YouTube = ({ videoId, style, play, origin }) => {
   return (
     <iframe
       title="PMU"
-      src={`https://www.youtube.com/embed/${videoId}`}
+      src={`https://www.youtube.com/embed/${videoId}?autoplay=${play ? 1 : 0}&origin=${origin}`}
       style={style}
       frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
